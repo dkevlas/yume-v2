2941c79ab9b435cabb53a1f8a84de71082d16abd
Kevin <dennisk.blas@gmail.com>
2024-12-18 23:48:59 -0500

    axios error - not found

--- client/src/libs/AxiosError.ts
+++ client/src/libs/AxiosError.ts
@@ -3,14 +3,10 @@ import axios from "axios"
 export const AxiosError = (err: any) => {
     if(err instanceof axios.AxiosError){
         if(err.response){
-            return {
-                errorResponse: err
-            }
+            return err.response.data
         }
         if(err.request){
-            return {
-                errorRequest: err
-            }
+            return err.request
         }
     } else{
         return {
--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,7 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { GhostComp } from "../components/icos/IcosNotFoundComp";
+
 export default function NotFound(){
+    const navigate = useNavigate()
     return (
-        <h1 className="bg-red-300 text-red-600 text-4xl">
-            CODE 404
-        </h1>
+        <section 
+            className="
+                bg-yume-neutral-bold flex relative flex-col justify-center items-center gap-6 dark:bg-yume-deep-muted min-h-[clamp(30rem,_24.205rem_+_15.455vw,_40.625rem)] text-red-600 text-4xl"
+            >
+            <span className="dark:text-yume-secondary text-yume-primary text-[clamp(3.125rem,_2.375rem_+_3vw,_5rem)]">
+                Error 404
+            </span>
+            <div className="w-52">
+                <GhostComp />
+            </div>
+            <button 
+                className="
+                    absolute right-4 bottom-4 text-xl outline outline-2 opacity-75 hover:opacity-100 active:scale-95
+                    text-yume-primary dark:text-yume-secondary
+                    outline-yume-primary dark:outline-yume-secondary px-2
+                "
+                onClick={()=>{
+                    navigate('/')
+                }}
+            >
+                Ir al inicio
+            </button>
+        </section>
     )
 }
